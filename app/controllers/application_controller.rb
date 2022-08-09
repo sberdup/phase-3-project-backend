27638a58bcf2f343e542f29614125ffaf49c844c
Sergio Berdeja <sergioberdeja@gmail.com>
2022-08-09 17:15:19 -0600

    can access models through application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,16 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    'No data here!'.to_json
   end
 
+  get '/all' do
+    Entity.all.to_json
+  end
+  
+  get '/category/:category' do
+    category = Category.find_by(name:params[:category].to_s)
+    category.entities.to_json
+  end 
 end
